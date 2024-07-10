9825d372a70690564b3018ec806a23906d2e0a87
SHAHAN KHAN <102419339+123shahan@users.noreply.github.com>
2024-07-10 15:31:48 +0530

    Add files via upload
    js to solve the string problems.

---
+++ string.js
@@ -0,0 +1,31 @@
+let a = 'shahan';
+let b = 'kaif';
+
+//for convert string to uppercase
+console.log(a.toUpperCase());
+console.log(b.toUpperCase());
+// for same as lowercase
+
+// for concatenate means milana
+console.log(a.concat(" ",b));
+
+//repeat karana ho to
+
+console.log(a.repeat(3));
+
+//for replace the a = shahan
+console.log(a.replace("shahan","mohammad"));
+
+//if we find the length of the string 
+
+console.log(a.length);
+console.log(b.length)
+
+//if you want to find the char at particular index
+
+console.log(a.charAt(2));
+
+//if you want to slice 
+//shahan
+console.log(a.slice(1,2));
+
